246069c48d0845443c99c3c11b23507e82d3f1bd
PRAKHAR NAGAR <109655744+PRAKHARNAGAR2003@users.noreply.github.com>
2023-06-14 07:17:12 +0530

    Add files via upload

---
+++ Program-1.R
@@ -0,0 +1,33 @@
+# Data processing 
+# importing the data
+
+dataset = read.csv('Data.csv')
+
+# taking care of the empty data
+dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
+# Encoding the categorical Data
+
+dataset$Country = factor(dataset$Country,
+                         levels = c('France','Spain','Germany'),
+                         labels = c(1,2,3))
+                                          
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No','Yes'),
+                           labels = c(0,1))
+
+#splitting the data set into training set and test set
+# install.packages('caTools')
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset,split == TRUE)
+test_set = subset(dataset,split == FALSE)
+
+#Feature Scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
